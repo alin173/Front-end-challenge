4379de14b322ecfbe956d776864b29b3100e61a0
Andrew Lin <64857537+alin173@users.noreply.github.com>
2021-03-26 03:25:36 -0700

    Final build

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import mockData from "./content.json";
 import Navbar from "./components/Navbar";
 
 function App() {
-  
-  const [details, setDetails] = useState(mockData.pages[0])
+  const [pageData, setpageData] = useState(mockData.pages[0]);
+  const dataBlocks = pageData.blocks[0];
 
   return (
-    <div className="main">
-      <Navbar onDetailsChange={setDetails}/>
-      <div className="marquee">
-        <div className="content">
-          <div className="headline">
-            <h1>{details.title}</h1>
+    <div
+      className="page-container"
+      style={{
+        backgroundImage: `url(${
+          process.env.PUBLIC_URL + "/images/" + dataBlocks.background
+        })`,
+      }}
+    >
+      <div className="main">
+        <Navbar setpageData={setpageData} />
+        <div className="marquee">
+          <div className="content">
+            <div className="headline">
+              <h1>{pageData.title}</h1>
+            </div>
+
+            <div className="subhead">{dataBlocks.subhead}</div>
           </div>
 
-          <div className="subhead">{details.blocks[0].subhead}</div>
-        </div>
-
-        <div className="cta-container">
-          <div className="cta-text">
-          {details.blocks[0].cta}
-          </div>
-          <div className="cta-button">    
-          LETS TALK &#x2192;        
+          <div className="cta-container">
+            <div className="cta-text">{dataBlocks.cta}</div>
+            <div className="cta-button-container">
+              <button className="cta-button">
+                <div className="cta-button-text">LETS TALK.</div>
+                <div className="cta-button-arrow">&#x2192;</div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,32 +2,31 @@ import React from "react";
 import "../App.css";
 import mockData from "../content.json";
 
-function Navbar(onDetailsChange) {
+function Navbar({setpageData}) {
 
-  const test =()=>{
-    onDetailsChange(mockData.pages[0])
-  }
+ 
   return (
       <div className="nav">
         <div className="nav-container">
           <div className="header"> 
             <div className="logo">
-              <img className="logo-image" src="https://i.imgur.com/2RKmLSk.png" />
+              <img className="logo-image" alt="logo" src={`${process.env.PUBLIC_URL}/images/abclogo.png`} />
             </div>
             <div className="menu">
-              <button onClick={test}>Industries</button>
-              <button>Services</button>
-              <button>About Us</button>
+              <button onClick={() => {setpageData(mockData.pages[0])}}>Industries</button>
+              <button onClick={() => {setpageData(mockData.pages[1])}}>Services</button>
+              <button onClick={() => {setpageData(mockData.pages[2])}}>About Us</button>
             </div>
           </div>
 
           <div className="contact">
-            <button id="contact">Contact Us</button>
+            <button id="contact-button">Contact Us</button>
           </div>
         </div>
       </div>
     
   );
 }
+
 
 export default Navbar;
